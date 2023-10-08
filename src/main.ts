84bb38896cdb9b3a2a5b327cf8344a561e1f5972
Kaburi <specialemail90@gmail.com>
2023-10-08 17:46:25 +0900

    cors 추가

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://localhost:3001'], // 허용할 출처    
+    origin: ['http://localhost:3001', 'https://localhost:3001', "https://web-product-shop-client-euegqv2blndv3jgq.sel5.cloudtype.app/login"], // 허용할 출처    
     credentials: true,
   });
 
