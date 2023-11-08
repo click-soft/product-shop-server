9af7106b05a2dada2487bc4e7797680d40a8d862
Kaburi <specialemail90@gmail.com>
2023-11-08 11:34:50 +0900

    .

--- src/api/account/dto/change-password.args.ts
+++ src/api/account/dto/change-password.args.ts
@@ -1,10 +1,12 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsPassword } from 'src/validators/password.validator';
 
 @ArgsType()
 export default class ChangePasswordArgs {
   @Field()
   userId: string;
 
+  @IsPassword()
   @Field()
   password: string;
 }
